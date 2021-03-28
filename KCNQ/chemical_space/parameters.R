3eda852ecd2a4333792b7c7b987890dd627604f4
Matthew O'Meara <maom@gl-login2.arc-ts.umich.edu>
2021-03-28 12:31:17 -0400

    KCNQ chemical_space work on make decoys embedding work

--- KCNQ/chemical_space/parameters.R
+++ KCNQ/chemical_space/parameters.R
@@ -31,7 +31,8 @@ get_parameters <- function(
         project_data_googlesheets_id = "1a7Y93LkvcgQkpqhmQN2CmRNdCjnJiRgIB6IaIaJktKw"
     )
 
-    if (system_id == "maom@gl-login1.arc-ts.umich.edu") {
+    if (system_id == "maom@gl-login1.arc-ts.umich.edu" ||
+        system_id == "maom@gl-login2.arc-ts.umich.edu") {
         parameters <- c(
             parameters,
             featurize_substances_program = "~/opt/miniconda3/envs/mplearn/bin/featurize_substances",
--- KCNQ/chemical_space/scripts/2_embed_substances.R
+++ KCNQ/chemical_space/scripts/2_embed_substances.R
@@ -20,6 +20,18 @@ cat(cmd, sep = "")
 system(cmd)
 
 
+cmd <- paste0(
+parameters$featurize_substances_program, " \\
+  --library_path intermediate_data/project_decoys_20210323.tsv \\
+  --substance_id_field 'substance_zinc_id' \\
+  --smiles_field 'substance_smiles' \\
+  --output_path intermediate_data/project_decoys_20210323 \\
+  --verbose
+")
+cat(cmd, sep = "")
+system(cmd)
+
+
 
 expand.grid(
     a = c(1),
@@ -29,15 +41,17 @@ expand.grid(
         params <- .
         alt_tag <- paste0("project_substances_a=", params$a, ",b=", params$b, "_", date_code)
         cat("Computing embedding for: ", alt_tag, "\n", sep = "")
-        command <- paste0("
-            ", parameters$embed_umap_program, " \\
-              --dataset intermediate_data/", tag, "/fingerprints.parquet \\
-              --feature_columns intermediate_data/", tag, "/fingerprint_feature_columns.tsv \\
-              --tag ", alt_tag, " \\
-              --umap_a ", params$a, " \\
-              --umap_b ", params$b, " \\
-              --verbose
-        ", sep = "")
+        command <- paste0(
+            parameters$embed_umap_program, " ",
+            "--dataset ",
+            "intermediate_data/project_substances_20210323/fingerprints.parquet ",
+            "intermediate_data/project_decoys_20210323/fingerprints.parquet ",
+            "--feature_columns intermediate_data/", tag, "/fingerprint_feature_columns.tsv ",
+            "--tag ", alt_tag, " ",
+            "--umap_a ", params$a, " ",
+            "--umap_b ", params$b, " ",
+            "--verbose",
+        sep = "")
         cat(command, sep = "")
         system(command)
         data.frame()
